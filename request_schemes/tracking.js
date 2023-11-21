64a915ca668db2e55341911b90c15ec0b3dc8045
Ryan Murphy <ryanm@vogelheating.com>
2023-11-21 08:07:04 -0600

    fleshed out tracking scheme

--- request_schemes/tracking.js
+++ request_schemes/tracking.js
@@ -1,7 +1,22 @@
 getTrack = {
-    name:'getTrack',
-    check:false,
-    scheme:{
-        id:{type:String,default:false,msg:'The id of the tracker'}
+    name: 'getTrack',
+    check: false,
+    scheme: {
+        id: { type: String, default: false, msg: 'The ID of the tracked item' },
+        projectname: { type: String, default: false, msg: 'The NAME of the associated Project' },
+        client: { type: String, default: false, msg: 'The NAME of the client' },
+        street: { type: String, default: false, msg: 'The STREET ADREESS of the client' },
+        city: { type: String, default: false, msg: 'The CITY NAME of the client' },
+        zip: { type: String, default: false, msg: 'The ZIP CODE of the client' },
+        phone: { type: String, default: false, msg: 'The PHONE NUMBER of the client' },
+        email: { type: String, default: false, msg: 'The EMAIL ADDRESS of the client' },
+        estimator: { type: String, default: false, msg: 'The ESTIMATOR on the Project' },
+        stage: { type: String, default: false, msg: 'What STAGE the associated Project is in' },
+        status: { type: String, default: false, msg: 'What the STATUS of the associated Project is' },
+        date: { type: Date, default: false, msg: 'The initial APPOINTMENT DATE' },
+        prstdate: { type: Date, default: false, msg: 'The PRESENTATION DATE when the quote was given' },
+        bids: { type: Array, default: false, msg: 'List of BIDS presented' },
+        ref: { type: Array, default: false, msg: 'List of QUOTE ID REFERENCES that are associated with this item' },
+        bookprc: { type: Boolean, default: false, msg: 'Whether or not the BOOK PRICE was used' },
     },
 }
