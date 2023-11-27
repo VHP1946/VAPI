69c7940510b090d935016fd94e4b2915ed778233
vhc1946 <christianv@vogelheating.com>
2023-11-27 11:07:40 -0600

    finishes on the testing module '

--- testing/server-tester.js
+++ testing/server-tester.js
@@ -1,20 +1,45 @@
 let readline = require('readline');
 let modules = require('./request_packs/pack_selector.js');
-const { exec, execFileSync } = require('child_process');
-let Core = require('vhp-api')
+const {exec} = require('child_process');
+let {Core} = require('vhp-api')
+
+let runner = new Core({
+    host:'http://localhost:5000/',
+    dev:{comments:false,https:false}
+})
+
+if(false){
+    runner.SENDrequest({
+    }).then(response=>{
+        console.log('Response >',response,'\n\n')
+    })
+}
+
 
 //start new console
 //run server on that console
-
-var startup = async () => {
-    //await exec('start npm run start');
-    
-}
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+
+let askForServer = (question ='Do you need a server? ')=>{
+    return new Promise((resolve,reject)=>{
+        rl.question(question,answr=>{
+            if(answr=='yes'){//start another server
+                if(process.platform=='linux'){
+                    exec(`gnome-terminal -x bash -c "npm run dev; exec bash"`,(stderr)=>{
+                        console.log(stderr)
+                    });//run npm run test')
+                }else{
+                    exec('start npm run dev')
+                }
+            }
+            RUNroute()
+        })
+    })
+}
 
 let askForType = (question = 'What Request Type? -> ') => {
     return new Promise((resolve, reject) => {
@@ -25,37 +50,27 @@ let askForType = (question = 'What Request Type? -> ') => {
         })
     })
 }
+
 let RUNroute = (question = 'Run which route? -> ') => {
     rl.question(question, answrRoute => {
 
-        rl.write(`\nRunning route -> ${answrRoute}`);
         askForType().then(answrType => { // ask default or custom request pack
             let req = modules.selector(answrRoute, answrType);
             if (req) {
-                console.log('Request Route >', req);
-
-                let addy = 'http://localhost';
-                let port = '5000';
-                let full = addy + ':' + port + '/api/' + 'PING';
-                
-                exec(`start cmd /c curl -url ${full}`);
-                /*
-                fetch(`${full}`).then(resp=>{
-                    console.log(resp);
-                    RUNroute(question);//ask again
-                });
-                */
-                
-                // run request
-                // checker
-                // write response {success:Boolean, response:Object}
-                
+                console.log('Request >',answrRoute)
+                runner.SENDrequest({
+                    route:answrRoute,
+                    pack:req
+                }).then(response=>{
+                    console.log('Response >',response,'\n\n')
+                    RUNroute()
+                })
             } else {
                 console.log('Request was not found')
-                RUNroute(question);//ask again
+                RUNroute();//ask again
             }
         })
     })
 }
 
-RUNroute();
+ askForServer();
