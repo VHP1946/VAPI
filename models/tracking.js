eae6fe06e320f40daa7be49610564b8b00453ddd
vhc1946 <christianv@vogelheating.com>
2023-11-27 23:49:59 -0600

    tracking route and config folder chagne

--- models/tracking.js
+++ models/tracking.js
@@ -1,18 +1,16 @@
 
 /**
  * takes in data OR handler?
- * 
+ *
  * with data collects dependant data
  */
 getTrack = (data)=>{
     return new Promise((resolve,reject)=>{
         //get this from db
         //get that from db
-        
+
         //further prep on stuff
 
         return resolve(pack)
     })
 }
-
-
