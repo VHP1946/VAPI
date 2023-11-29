5423322352d049f1197385d7665db53649a20423
Ryan Murphy <ryanm@vogelheating.com>
2023-11-29 12:16:26 -0600

    fleshing out schemes

--- request_schemes/index.js
+++ request_schemes/index.js
@@ -1,7 +1,18 @@
-let TrackingSchemes = require('./TrackingSchemes.js');
+let AnalyticsSchemes = require('./AnalyticsSchemes.js');
+let BackupsSchemes = require('./BackupsSchemes.js');
+let PricingSchemes = require('./PricingSchemes.js');
 let ProjectSchemes = require('./ProjectSchemes.js');
+let SettingsSchemes = require('./SettingsSchemes.js');
+let SupportSchemes = require('./SupportSchemes.js');
+let TrackingSchemes = require('./TrackingSchemes.js');
+
 
 module.exports = {
-    ...TrackingSchemes,
-    ...ProjectSchemes
+    ...AnalyticsSchemes,
+    ...BackupsSchemes,
+    ...PricingSchemes,
+    ...ProjectSchemes,
+    ...SettingsSchemes,
+    ...SupportSchemes,
+    ...TrackingSchemes
 }
